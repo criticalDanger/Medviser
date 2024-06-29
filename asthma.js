0acbd0417d402848237d95a9aca7410e1311df73
criticalDanger <forskoolworks@gmail.com>
2024-06-29 21:01:15 +0530

    Add files via upload

---
+++ asthma.js
@@ -0,0 +1,6 @@
+function gotoChat() {
+    window.location.href = './'
+}
+function gotoDiagnose() {
+    window.location.href = './diagnose'
+}
---
+++ diagnose.js
@@ -0,0 +1,12 @@
+function gotoChat() {
+    window.location.href = './'
+}
+function gotoDiagnose() {
+    window.location.href = './diagnose'
+}
+function gotoHeart() {
+    window.location.href = './heart'
+}
+function gotoAsthma() {
+    window.location.href = './asthma'
+}
